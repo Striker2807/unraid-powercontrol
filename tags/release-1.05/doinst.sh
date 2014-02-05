1615d38ead7530617958c0546ca76746f45e87b1
danlandon2011@gmail.com <danlandon2011@gmail.com@34c33f3c-a37d-11dd-a9c7-49dec45ca499>
2014-02-05 12:40:59 +0000

    Release 1.05 of the 'unraid-powercontrol' project.

---
+++ tags/release-1.05/doinst.sh
@@ -0,0 +1,95 @@
+
+config() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+if [ ${DEBUG:=0} -gt 0 ]
+   then set -x -v 
+fi
+
+#for i in config.new 
+#do # config etc/$i; 
+#done
+
+SD_RCFILE=/etc/rc.d/rc.local_shutdown
+RCFILE=/etc/rc.d/rc.unRAID
+
+if ! grep ${RCFILE} ${SD_RCFILE} >/dev/null 2>&1
+   then echo -e "\n\n[ -x ${RCFILE} ] && ${RCFILE} stop\n" >> ${SD_RCFILE}
+fi
+
+[ ! -x ${SD_RCFILE} ] && chmod u+x ${SD_RCFILE}
+
+
+if [ ! -z "${CTRLALTDEL}" ];then 
+   sed --in-place=.bak -e 's/shutdown .*/powerdown/g' /etc/inittab
+   /sbin/telinit q
+fi
+
+if [ ! -z "${START}" ];then 
+   ${RCFILE} start
+fi
+
+if [ ! -z "${SYSLOG}" ];then 
+   ${RCFILE} syslog
+fi
+
+if [ ! -z "${STATUS}" ];then 
+   ${RCFILE} status
+fi
+
+if [ ! -z "${HDPARM}" ];then 
+   sed --in-place=.bak -e "s/HDPARM=.*/HDPARM=${HDPARM}/g" ${RCFILE}
+fi
+
+if [ ! -z "${SMARTCTL}" ];then 
+   sed --in-place=.bak -e "s/SMARTCTL=.*/SMARTCTL=${SMARTCTL}/g" ${RCFILE}
+fi
+
+if [ ! -z "${LOGDIR}" ];then 
+   sed --in-place=.bak -e "s/LOGDIR=.*/LOGDIR=${LOGDIR}/g" ${RCFILE}
+fi
+
+if [ ! -z "${LOGSAVE}" ];then 
+   sed --in-place=.bak -e "s/LOGSAVE=.*/LOGSAVE=${LOGSAVE}/g" ${RCFILE}
+fi
+
+if [ ! -z "${LOGROTATE}" ];then 
+   if ! grep ${RCFILE} /etc/logrotate.conf > /dev/null 2>&1
+      then IFS="
+"
+           echo "Fixing broken /etc/logrotate.conf"
+           sed --in-place=.bak -e 's/10K/10k/g' /etc/logrotate.conf
+           BLINE='/bin/kill -HUP cat /var/run/syslogd.pid.*'
+           GLINE='/bin/kill -HUP `cat /var/run/syslogd.pid 2>/dev/null` 2>/dev/null || true'
+           sed --in-place=.bak -e "s#${BLINE}#${GLINE}#g" /etc/logrotate.conf
+           while read LINE
+           do echo "${LINE}"
+              if [ "${LINE}" = "/var/log/syslog {" ]
+                 then echo "    prerotate"
+                      echo "       ${RCFILE} syslog"
+                      echo "    endscript"
+                      echo "    compress"
+              fi
+            done < /etc/logrotate.conf    > /etc/logrotate.conf.tmp
+            cat < /etc/logrotate.conf.tmp > /etc/logrotate.conf
+            rm -f /etc/logrotate.conf.tmp
+   fi
+fi
+
+
+# Joe L. & Weebotech Addition for power handler script.
+# sysctl -w kernel.poweroff_cmd="/sbin/powerdown"
+
+# http://lime-technology.com/forum/index.php?topic=2068.msg18287#msg18287
+# Joe L's mechanism for chaning the power handler"
+# sed -i -e "s/init 0/powerdown/" /etc/acpi/acpi_handler.sh   
